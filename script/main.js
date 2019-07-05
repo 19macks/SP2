92b51cb181ed9fcd8cd0c4e0462c5d4ca7579454
Artrm Panasenko <artem253510@gmail.com>
2019-07-05 11:35:16 +0300

    Work on forms

--- script/main.js
+++ script/main.js
@@ -1,30 +1,42 @@
-function toggle(el) {
-    el.style.display = (el.style.display === 'none') ? 'block' : 'none'
-}
 
-let request_doctor = document.querySelector(".apply_to_doctor");
-let btnNewReq = document.querySelector(".button");
-
+//Активировать форму выбора врача
+const request_doctor = document.querySelector(".apply_to_doctor")
+const btnNewReq = document.querySelector(".button")
 btnNewReq.addEventListener('click', () => {
     if (request_doctor.getAttribute('class') === "apply_to_doctor" ) {
         request_doctor.classList.add('active')
     }
 })
+//Выбрать определенного врача
+const selectDoctor = document.querySelector('#select-doctor')
+const btnCreatRequest = document.querySelector('.btn-creat-request')
+const forms = document.querySelectorAll('.form-request')
+const inputForm = document.querySelectorAll('.input-form')
+selectDoctor.addEventListener('click',function () {
+    if (this.value) {
+        forms.forEach((form)=>{
+            if (form.getAttribute('data-doc') === this.value) {
+                form.classList.add('active')
+                btnCreatRequest.classList.add('active')
 
-let selectDoctor = document.querySelector('#select-doctor')
-let doctors = document.querySelectorAll('.doctor');
-
-selectDoctor.addEventListener('click', (event) => {
-    let doctors = document.querySelectorAll('.doctor')
-    let forms = document.querySelectorAll('.form-request')
-    console.log(event.target);
-    // if (event.target.getAttribute('data')  ) {
-    //     doctors.getAttribute()
-    // }
-    //
-    // eve
-    // doctors.forEach((el) => {
-    //
-    // })
-
+                inputForm.forEach((input) => {
+                    input.value = ''
+                })
+            } else {
+                form.classList.remove('active')
+            }
+        })
+    }
+})
+//Закрыть крестиком форму выбора врача с обнулением инпутов
+const btnClosed = document.querySelector('.fa-times')
+btnClosed.addEventListener('click', function () {
+    request_doctor.classList.remove('active')
+    inputForm.forEach((input) => {
+        input.value = ''
+    })
+    forms.forEach((form) => {
+        form.classList.remove('active')
+    })
+        selectDoctor.value = 'select'
 })
