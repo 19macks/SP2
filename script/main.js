ace05db0aa41b1f4dfe7330dac0ebd3126c8c715
19macks <19macks@gmail.com>
2019-07-10 18:34:20 +0300

    visible form

--- script/main.js
+++ script/main.js
@@ -3,16 +3,33 @@ let closeButton = document.querySelector(".close_button");
 let form = document.querySelector(".apply_to_doctor");
 let select = document.getElementById('select');
 
-
-button.onclick = function () {
-    form.classList.add("active");
-
-};
-
-
 closeButton.onclick = function () {
     form.classList.remove("active")
 };
+
+document.body.addEventListener("click", function (event) {
+    let target = event.target;
+    console.log(target);
+
+    while (target.tagName !== 'BODY') {
+        if (target === form) {
+            form.classList.add("active");
+            return;
+        }
+        else if (target === button) {
+            form.classList.add("active");
+            return;
+        }
+        else if (target === closeButton) {
+            form.classList.remove("active");
+            return;
+        }
+        else {
+            form.classList.remove("active")
+        }
+        target = target.parentNode;
+    }
+});
 
 select.onchange = function () {
     if (this.value === "therapist")  {
@@ -34,14 +51,10 @@ select.onchange = function () {
 };
 
 
-
-
-
 function removeActiveOfFormRequest() {
     Array.from(document.getElementsByClassName("form-request")).forEach(function (elem) {
         elem.classList.remove("active")
     });
 }
-
 
 
