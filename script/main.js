e4afa54a41c97e2f984772b89a02b5455a0d5330
19macks <19macks@gmail.com>
2019-07-15 19:17:49 +0300

    important work

--- script/main.js
+++ script/main.js
@@ -2,9 +2,14 @@ let button = document.querySelector(".button");
 let closeButton = document.querySelector(".close_button");
 let form = document.querySelector(".apply_to_doctor");
 let select = document.getElementById('select');
+let requestButton = document.querySelector(".btn-creat-request");
+let inputs = document.querySelectorAll(".input-form");
+let cardiologistForms = document.querySelectorAll(".form-cardiologist");
+let therapistForms = document.querySelectorAll(".form-therapist");
+let dentistForm = document.querySelectorAll(".form-dentist");
 
 closeButton.onclick = function () {
-    form.classList.remove("active")
+    resetForm();
 };
 
 document.body.addEventListener("click", function (event) {
@@ -17,6 +22,11 @@ document.body.addEventListener("click", function (event) {
             return;
         }
         else if (target === button) {
+            inputs.forEach(function (form) {
+                form.value = "";
+            });
+            select.value = "select_doctor";
+            document.querySelector(".container-for-form").classList.remove("active");
             form.classList.add("active");
             return;
         }
@@ -51,10 +61,76 @@ select.onchange = function () {
 };
 
 
+////// C L A S S E S /////////
+class Visit {
+    constructor(doctor, name, surname, patronymic, dateOfVisit, purpose) {
+        this._doc = doctor;
+        this._userName = name;
+        this._userSurname = surname;
+        this._userPatronymic = patronymic;
+        this._currentDate = dateOfVisit;
+        this._target = purpose;
+    }
+}
+
+class Therapist extends Visit {
+    constructor(doctor, name, surname, patronymic, dateOfVisit, purpose, age) {
+        super (doctor, name, surname, patronymic, dateOfVisit, purpose);
+        this._userAge = age;
+    }
+}
+
+class Cardiologist extends Visit {
+    constructor(doctor, name, surname, patronymic, dateOfVisit, purpose, pressure, bmi, diseasesCS, age) {
+        super (doctor, name, surname, patronymic, dateOfVisit, purpose);
+        this._userPressure = pressure;
+        this._userBMI = bmi;
+        this._userDiseasesCS = diseasesCS;
+        this._age = age;
+    }
+}
+
+class Dentist extends Visit {
+    constructor(doctor, name, surname, patronymic, dateOfVisit, purpose, lastVisit) {
+        super(doctor, name, surname, patronymic, dateOfVisit, purpose);
+        this._lastvis = lastVisit;
+    }
+}
+
+requestButton.onclick = function () {
+    if (select.value === "therapist") {
+        let doctor = "Терапевт";
+        let name = document.querySelector(".name-therapist").value;
+        let surname = document.querySelector(".surname-therapist").value;
+        let patronymic = document.querySelector(".patronymic-therapist").value;
+        let dateOfVisit = document.querySelector(".date-therapist").value;
+        let purpose = document.querySelector(".purpose-therapist").value;
+        let age = document.querySelector(".age-therapist").value;
+        if (name && surname && patronymic && dateOfVisit && purpose && age !== false) {
+            let visitTherapist = new Therapist(doctor, name, surname, patronymic, dateOfVisit, purpose, age);
+            console.log(visitTherapist);
+        }
+    }
+};
+
+
+/////// F U N C T I O N S /////////
 function removeActiveOfFormRequest() {
     Array.from(document.getElementsByClassName("form-request")).forEach(function (elem) {
         elem.classList.remove("active")
     });
 }
+
+function resetForm() {
+    inputs.forEach(function (form) {
+        form.value = "";
+    });
+    select.value = "select_doctor";
+    document.querySelector(".container-for-form").classList.remove("active");
+}
+
+
+
+
 
 
